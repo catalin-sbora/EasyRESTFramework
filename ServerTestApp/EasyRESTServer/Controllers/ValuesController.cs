b87ba52555d691bcb070cd3d207a2e6f6449c713
Catalin Sbora <catalin.sbora@gmail.com>
2017-05-01 09:02:49 -0700

    - added a new value for test

--- ServerTestApp/EasyRESTServer/Controllers/ValuesController.cs
+++ ServerTestApp/EasyRESTServer/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace EasyRESTServer.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
